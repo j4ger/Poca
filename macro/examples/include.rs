64bda98760f4eae7494cd44b58ea581387b72e3b
j4ger <xiayuxuan@live.com>
2022-02-16 11:11:19 +0800

    Add macro examples;

---
+++ macro/examples/include.rs
@@ -0,0 +1,9 @@
+use poca_macro::include_app_dir;
+
+fn main() {
+    // note for myself:
+    // to avoid using `let` in macro quotes,
+    // design only one function that takes a vec of (string,bytes,option<(vec)>) tuples
+    let app_routes = include_app_dir!("examples/public/");
+    println!("Hi from macro");
+}
